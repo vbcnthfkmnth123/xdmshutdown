90822d30e26ce5f83cf0e7bcdab1c830e0d452d8
vbcnthfkmnth123 <135506663+vbcnthfkmnth123@users.noreply.github.com>
2024-02-14 12:15:30 +0000

    Update xdmshutdown.c
    Test with doas

--- xdmshutdown.c
+++ xdmshutdown.c
@@ -67,12 +67,12 @@ int main(int argc, char **argv)
 void b_shutdownPushed(Widget w, XtPointer client_data,
 					  XmPushButtonCallbackStruct *cbs)
 {
-	system("/usr/bin/sudo /sbin/poweroff");
+	system("/usr/bin/doas /sbin/poweroff");
 }
 
 void b_rebootPushed(Widget w, XtPointer client_data,
 					XmPushButtonCallbackStruct *cbs)
 {
-	system("/usr/bin/sudo /sbin/reboot");
+	system("/usr/bin/doas /sbin/reboot");
 }
 
